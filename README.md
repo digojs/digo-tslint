a67dba4b4d2432d414684454a3f3e01b402414b1
xuld <xuld@vip.qq.com>
2017-10-10 12:53:39 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,26 @@
+﻿# digo-tslint
+[digo](https://github.com/digojs/digo) 插件：使用 [TSLint](https://palantir.github.io/tslint/) 检测 TypeScript 代码。
+
+## 安装
+```bash
+npm install digo-tslint -g
+```
+
+## 用法
+### 检测 TypeScript 代码
+```js
+digo.src("*.ts", "*.tsx").pipe("digo-tslint");
+```
+
+## 选项
+```js
+digo.src("*.ts", "*.tsx").pipe("digo-clean-css", {
+    level: "error",                 // 报错等级。可以是 "error" 或 "warning"。
+    configuration: "tslint.json",   // 规则文件。详见 https://palantir.github.io/tslint/usage/configuration/
+    program: null,                  // 如果需要启动类型检查，则指定 tsconfig.json 路径或 ts.Program 对象。
+    rulesDirectory: null,           // 规则文件夹。
+    cwd: null                       // 根文件夹。
+});
+```
+
+另参考 https://palantir.github.io/tslint/rules/。
---
+++ index.js
@@ -0,0 +1,48 @@
+﻿var tslint = require("tslint");
+
+var programs = {};
+
+module.exports = function TSLint(file, options) {
+    var configuration = options.configuration;
+    if (configuration == null || typeof configuration === "string") {
+        configuration = tslint.Configuration.findConfiguration(configuration, file.srcPath).results;
+    } else {
+        configuration = tslint.Configuration.parseConfigFile(configuration, options.cwd);
+    }
+    var program = options.program;
+    if (program == null) {
+        program = tslint.Linter.findConfigurationPath("tsconfig.json");
+    }
+    if (typeof program === "string") {
+        const key = (options.cwd || "") + "/" + program;
+        program = programs[key] || (programs[key] = tslint.Linter.createProgram(program, options.cwd));
+    }
+
+    var linter = new tslint.Linter({
+        fix: options.fix || false,
+        formatter: options.formatter || "prose",
+        formattersDirectory: options.formattersDirectory || null,
+        rulesDirectory: options.rulesDirectory || null
+    }, program);
+    linter.lint(file.srcPath, file.content, configuration);
+
+    var result = linter.getResult();
+    for (const failure of result.failures) {
+        const startLoc = failure.getStartPosition().getLineAndCharacter();
+        const endLoc = failure.getEndPosition().getLineAndCharacter();
+        const e = {
+            plugin: TSLint.name,
+            message: failure.getRuleName() + ": " + failure.getFailure(),
+            fileName: failure.getFileName(),
+            line: startLoc.line,
+            column: startLoc.character,
+            endLine: endLoc.line,
+            endColumn: endLoc.character
+        };
+        if (options.level === "warning" || failure.getRuleSeverity() === "warning") {
+            file.warning(e);
+        } else {
+            file.error(e);
+        }
+    }
+};
